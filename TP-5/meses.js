6f2e8dac823c8c9930df1c0eea76a43cc74ce1b6
juanpabloch <juanpablochoter@gmail.com>
2021-03-25 12:39:08 -0300

    TP-5 JavaScript v2

--- TP-5/meses.js
+++ TP-5/meses.js
@@ -3,11 +3,18 @@ const meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "
     
 const diasDelMes= [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
-
+console.log("meses con 30 dias:")
 for (let i = 0; i < meses.length; i++) {
-    if(diasDelMes[i] >= 30){
-        console.log(meses[i] + " tiene " + diasDelMes[i] + " dias")
+    if(diasDelMes[i] === 30){
+        console.log(meses[i]);
     }
 }
 
-//hola 
+console.log(" ")
+
+console.log("meses con 31 dias:")
+for (let i = 0; i < meses.length; i++) {
+    if(diasDelMes[i] === 31){
+        console.log(meses[i]);
+    }
+}
